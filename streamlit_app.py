fe8ca88f6a563de31ae3bc8df6377f046a0b8818
felipe-manzanero <86502958+felipe-manzanero@users.noreply.github.com>
2022-09-21 20:58:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ dfCalificaciones = pd.read_excel("tareas.xlsx")
 
 dfCalificaciones["promedio"]=(dfCalificaciones["tarea1"]+dfCalificaciones["tarea2"]+dfCalificaciones["tarea3"]+dfCalificaciones["tarea4"])/4
 
-dfCalificaciones["status"]= df["promedio"].apply(lambda x: "APROBADO" if x>=70 else "REPROBADO")
+dfCalificaciones["status"]= dfCalificaciones["promedio"].apply(lambda x: "APROBADO" if x>=70 else "REPROBADO")
 
 dfResultados = dfCalificaciones[['matricula','promedio','status']]
 
